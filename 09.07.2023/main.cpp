175992041346d3327a62b875716e3ad901236c33
Rufat7 <alievrufat380@gmail.com>
2023-09-27 18:50:58 +0400

    HW

---
+++ 09.07.2023/main.cpp
@@ -0,0 +1,13 @@
+
+#include "header.h"
+
+int main() {
+    string num2;
+
+    cout << "Enter a number: ";
+    cin >> num2;
+
+    cout << "Entered number: " << Checking::calculate(num2) << endl;
+
+    return 0;
+}
