cd3e14592f28080cda3c98664e78c7ee344b1baf
Rufat7 <alievrufat380@gmail.com>
2024-04-09 01:13:15 +0400

    HW

---
+++ 08.02.2024/08.02.2024/ProgrammerAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper;
+    public class ProgrammerAction
+    {
+        private readonly string _connectionString;
+
+        public ProgrammerAction(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Programmer> GetAllProgrammers()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            return connection.Query<Programmer>("SELECT * FROM Cars");
+        }
+    }
+
