372a5b7ad48e4b080d68935e3e975cece8746216
Rufat7 <alievrufat380@gmail.com>
2024-04-09 01:11:21 +0400

    HW

---
+++ 11.01.2024/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO;
+
+public class Data
+{
+    public DataTable CreateTaskTable()
+    {
+        DataTable dataTable = new DataTable("Tasks");
+
+        dataTable.Columns.Add("Id", typeof(int));
+        dataTable.Columns.Add("Name", typeof(string));
+
+        return dataTable;
+    }
+}
---
+++ 11.01.2024/ITaskOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO;
+
+    public interface ITaskOperation
+{
+        public List<Task> GetAllTask();
+        public Task CreateTask(Task task);
+        public Task UpdateTask(Task task);
+        public bool DeleteTask(int taskId);
+    }
+
