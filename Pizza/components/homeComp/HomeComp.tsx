6635313f7c1b8167d89069973a8b838c2c591d50
Rufat7 <alievrufat380@gmail.com>
2025-04-01 02:29:43 +0400

    HW

--- Pizza/components/homeComp/HomeComp.tsx
+++ Pizza/components/homeComp/HomeComp.tsx
@@ -2,74 +2,74 @@ import React from "react";
 import styles from "./HomeComp.module.css";
 import Image from "next/image";
 
-function HomeComp() {
+function HeroSection() {
   return (
-    <div className={styles.home}>
-      <div className={styles.home_container}>
-        <div className={styles.home_leftSide}>
-          <div className={styles.home_leftSide_title}>
-            <span className={styles.home_leftSide_title_text}>The Fastest</span>
-            <div className={styles.home_leftSide_title_textWithIcon}>
+    <section className={styles.hero}>
+      <div className={styles.hero__container}>
+        <div className={styles.hero__left}>
+          <div className={styles.hero__title}>
+            <span className={styles.hero__titleMain}>The Fastest</span>
+            <div className={styles.hero__titleWithIcon}>
               <span>Pizza</span>
               <Image
                 src="/icons/Lightning.png"
-                alt=""
+                alt="Lightning icon"
                 width={50}
                 height={50}
-              ></Image>
+              />
               <span>Delivery</span>
             </div>
           </div>
-          <span className={styles.leftSide_desc}>
+          <p className={styles.hero__description}>
             We will deliver juicy pizza for your family in 30 minutes, if the
             courier is late -{" "}
-            <span className={styles.leftSide_descGray}>pizza is free!</span>
-          </span>
-          <div className={styles.leftSide_process}>
+            <span className={styles.hero__descriptionHighlight}>pizza is free!</span>
+          </p>
+          <div className={styles.hero__process}>
             <span>Cooking process </span>
             <Image
               src="/images/HomeComp1.png"
-              alt=""
-              className={styles.leftSide_process_img}
+              alt="Cooking process"
+              className={styles.hero__processImage}
               width={272}
               height={193}
-            ></Image>
+            />
           </div>
-          <div className={styles.leftSide_buttonsContainer}>
-            <div className={styles.leftSide_btnGroup}>
-              <button className={styles.leftSide_btnGroup_btn}>To order</button>
-              <button className={styles.leftSide_btnGroup_btn}>
-                <span className={styles.leftSide_btnGroup_btn_second}>Pizza-Menu</span>
+          <div className={styles.hero__cta}>
+            <div className={styles.hero__buttons}>
+              <button className={styles.hero__buttonPrimary}>To order</button>
+              <button className={styles.hero__buttonSecondary}>
+                <span className={styles.hero__buttonText}>Pizza-Menu</span>
               </button>
             </div>
           </div>
         </div>
-        <div className={styles.home_rightSide}>
+        <div className={styles.hero__right}>
           <Image
             src="/images/HomeComp2.png"
-            alt="HomeComp2"
-            className={styles.home_rightSide_mainImg}
+            alt="Pizza delivery"
+            className={styles.hero__mainImage}
             width={456}
             height={684}
-          ></Image>
+          />
           <Image
             src="/icons/fries.png"
-            alt=""
-            className={styles.home_rightSide_additionImg_fries}
+            alt="French fries"
+            className={styles.hero__decorationFries}
             width={212}
             height={226}
-          ></Image>
+          />
           <Image
             src="/icons/pizza.png"
-            alt=""
-            className={styles.home_rightSide_additionImg_pizza}
+            alt="Pizza"
+            className={styles.hero__decorationPizza}
             width={252}
             height={252}
-          ></Image>
+          />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
-export default HomeComp;
+export default HeroSection;
